40aa04f793bfd39475def9ca93e4f23015addf95
krkmo10 <cm0252032021@unab.edu.sv>
2021-11-08 20:50:12 -0600

    act.

---
+++ Parcial2/Parcial2/Controllers/RetiroDineroController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Parcial2.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parcial2.Controllers
+{
+    public class RetiroDineroController : Controller
+    {
+        // GET: RetiroDinero
+    
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public ActionResult IndexR()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IndexR(RetiroDinero retiroDineroCls)
+        { if (retiroDineroCls.monto % 5 == 0)
+            {
+                return Redirect("RetiroS");
+            }
+
+            return Redirect("RetiroN");
+        }
+
+        [HttpGet]
+        public ActionResult RetiroS()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RetiroN()
+        {
+
+            return View();
+        }
+    }
+
+}
---
+++ Parcial2/Parcial2/Models/RetiroDinero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2.Models
+{
+    public class RetiroDinero
+    {
+        public int monto { get; set; }
+
+
+    }
+}
